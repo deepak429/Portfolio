82ff05fea4fa795e961d9e5bbe41f5c4cdbf6544
deepak429 <deepakch429@gmail.com>
2024-10-10 22:29:26 +0530

    alalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
